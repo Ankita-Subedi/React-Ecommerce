a5ec1cfb7ca6e6f66c69d04917093a08ca7ee540
Ankita-Subedi <ankita7705@mbmcsit.edu.np>
2024-10-07 19:59:23 +0545

    deployment setup complete

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/React-Ecommerce/',
+  build: {
+    outDir: 'dist', // Default output directory for Vite
+  },
   plugins: [react()],
 })
