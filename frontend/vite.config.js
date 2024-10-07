11abf4185a4471ff03da182371f4a8d16b53300f
Ankita-Subedi <ankita7705@mbmcsit.edu.np>
2024-10-07 20:41:13 +0545

    app setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/React-Ecommerce/',
-  build: {
-    outDir: 'dist', // Default output directory for Vite
-  },
   plugins: [react()],
 })
